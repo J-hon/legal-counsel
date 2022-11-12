5e82da6f6af4042dbf3659b82e002850d2a2f880
J-hon <Jonadify007@gmail.com>
2022-11-12 21:08:49 +0100

    Adds profile picture reminder job

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
 {
@@ -21,5 +22,10 @@ class Client extends Model
         'profile_picture',
         'case_details'
     ];
+
+    public function reminder(): HasOne
+    {
+        return $this->hasOne(Reminder::class);
+    }
 
 }
